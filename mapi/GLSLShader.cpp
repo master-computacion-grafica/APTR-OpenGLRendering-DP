38a0a13be2bda6c8c7fd8e1aad7ee78dcd0f23f5
Pablo4lf <91669117+Pablo4lf@users.noreply.github.com>
2026-01-30 00:25:08 +0100

    FIX: GLSL Shader Fix

--- mapi/GLSLShader.cpp
+++ mapi/GLSLShader.cpp
@@ -2,72 +2,66 @@
 
 GLSLShader::GLSLShader(std::string fileName) : Program(fileName)
 {
-	this->fileName = fileName;
+    this->fileName = fileName;
 
-	if (fileName.ends_with(".frag"))
-	{
-		this->type = programTypes_e::fragment;
-	}
-	else if (fileName.ends_with(".vert"))
-	{
-		this->type = programTypes_e::vertex;
-	}
-	
-	GLenum programType;
+    if (fileName.ends_with(".frag"))
+    {
+        this->type = programTypes_e::fragment;
+    }
+    else if (fileName.ends_with(".vert"))
+    {
+        this->type = programTypes_e::vertex;
+    }
 
-	if (type == programTypes_e::vertex)
-		programType = GL_VERTEX_SHADER;
-	else if (type == programTypes_e::fragment)
-		programType = GL_FRAGMENT_SHADER;
-	this->idProgram=glCreateShader(programType);
-<<<<<<< Updated upstream
+    GLenum programType;
+    if (type == programTypes_e::vertex)
+        programType = GL_VERTEX_SHADER;
+    else if (type == programTypes_e::fragment)
+        programType = GL_FRAGMENT_SHADER;
+    this->idProgram = glCreateShader(programType);
 }
 
 
 void GLSLShader::compile()
 {
-=======
-	
->>>>>>> Stashed changes
-	GLint fileSize;
-	auto code = readFile(fileSize);
-	
-	glShaderSource(idProgram, 1, &code, &fileSize);
-	glCompileShader(idProgram);
-	
-	checkErrors();
+    GLint fileSize;
+    auto code = readFile(fileSize);
+
+    glShaderSource(idProgram, 1, &code, &fileSize);
+    glCompileShader(idProgram);
+
+    checkErrors();
 }
 
 
 void GLSLShader::checkErrors()
 {
-	GLint fragment_compiled;
-	glGetShaderiv(idProgram, GL_COMPILE_STATUS, &fragment_compiled);
-	if (fragment_compiled != GL_TRUE)
-	{
-		GLsizei log_length = 0;
-		GLchar message[1024];
-		glGetShaderInfoLog(idProgram, 1024, &log_length, message);
-		std::cout << "ERROR " << fileName << "\n" << message << "\n\n";
-		compiled = false;
-		return;
-	}
-	compiled = true;
+    GLint fragment_compiled;
+    glGetShaderiv(idProgram, GL_COMPILE_STATUS, &fragment_compiled);
+    if (fragment_compiled != GL_TRUE)
+    {
+        GLsizei log_length = 0;
+        GLchar message[1024];
+        glGetShaderInfoLog(idProgram, 1024, &log_length, message);
+        std::cout << "ERROR " << fileName << "\n" << message << "\n\n";
+        compiled = false;
+        return;
+    }
+    compiled = true;
 }
 
 
 char* GLSLShader::readFile(GLint& fileSize)
 {
-	std::ifstream f(fileName);
-	if (f.is_open()) {
-		char* code = std::string(std::istreambuf_iterator<char>(f), {}).data();
-		fileSize = std::strlen(code);
-		f.close();
-		return code;
-	}
-	
-	std::cout << "ERROR: FICHERO NO ENCONTRADO " <<
-		__FILE__ << ":" << __LINE__ << " " << fileName << "\n";
-	return nullptr;
-	
+    std::ifstream f(fileName);
+    if (f.is_open()) {
+        char* code = std::string(std::istreambuf_iterator<char>(f), {}).data();
+        fileSize = std::strlen(code);
+        f.close();
+        return code;
+    }
+
+    std::cout << "ERROR: FICHERO NO ENCONTRADO " << __FILE__ << ":" << __LINE__ << " " << fileName << "\n";
+    return nullptr;
+
 }
