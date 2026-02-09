13affc8211e276ecfff15d2e33b7e108267c0cea
Pablo4lf <91669117+Pablo4lf@users.noreply.github.com>
2026-02-09 22:22:29 +0100

    TODO: Camera Not Painting v2

--- mapi/APIS3D_2026.cpp
+++ mapi/APIS3D_2026.cpp
@@ -1,26 +1,3 @@
-//#define GLAD_BIN
-//#include "mapi/common.h"
-//#include "GLFWKeyManager.h"
-//#include "Object.h"
-//#include "Camera.h"
-//
-//using namespace std;
-//
-//vector<Object*> objectList; //lista de objetos en el mundo 3D
-//
-//void updateObjects(float timeStep, Camera& cam)
-//{
-//
-//	//por cada objeto
-//		//actualizar eventos
-//	cam.step(timeStep);
-//
-//	for (auto &obj : objectList)
-//	{
-//		obj->step(timeStep);
-//	}
-//}
-
 #define GLAD_BIN
 #define STB_IMAGE_IMPLEMENTATION
 #include "CameraFPS.h"
@@ -40,25 +17,27 @@ int main(int argc, char** argv)
 	// Inicializar la clase System
 	System::initSystem();
 
-	CameraFPS* cam = new CameraFPS(projectionType_e::KEYBOARD, glm::vec3(1.0f, 1.0f, 3.0f), glm::vec3(-1.0f, -1.0f, -3.0f), glm::vec3(0.0f, 1.0f, 0.0f), 1.0f);
+	CameraKeyboard* cam = new CameraKeyboard(projectionType_e::KEYBOARD, glm::vec3(1.0f, 1.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), 1.0f);
 	World* world = System::getWorld();
 	world->addCamera(cam);
 	world->setActiveCamera(world->getCameraIndex(cam));
 
 	//Crear objeto ciudad
-	//Object3D* town = new Object3D();
-	CubeTex* cube = new CubeTex();
+	Object3D* town = new Object3D();
 
 	// Cargar mallas desde archivo
-	//town->loadDataFromFile("./data/asian_town.msh");
+	town->loadDataFromFile("./data/asian_town.msh");
 	
-	//town->setPosition(glm::vec4(0,0,0,0));
-	//town->setRotation(glm::vec4(0,0,0,0));
-	//town->setScale(glm::vec4(1,1,1,1));
+	town->setPosition(glm::vec4(0,0,0,0));
+	town->setRotation(glm::vec4(0,0,0,0));
+	town->setScale(glm::vec4(1,1,1,1));
 
-	System::addObject(cube);
-	
-	System::mainLoop();
+	System::addObject(town);
+
+	while (!System::getEnd())
+	{
+		System::mainLoop();
+	}
 }
 
 //if (glfwInit() != GLFW_TRUE)
